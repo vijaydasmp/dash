88d85cc529d9fdbe71d0f764f78b6823566b669d
W. J. van der Laan <laanwj@protonmail.com>
2021-05-19 09:19:07 +0200

    Merge bitcoin/bitcoin#21985: net: Return IPv6 scope id in `CNetAddr::ToStringIP()`
    6c280adcd865ae3da4df53d630c9bf737283a56f net: Return IPv6 scope id in `CNetAddr::ToStringIP()` (W. J. van der Laan)
    
    Pull request description:
    
      If a scope id is provided, return it back in the string representation. Also bring back the test (now in platform independent fashion). Closes #21982. Includes #21961 (apart from the MacOS remark).
    
    ACKs for top commit:
      practicalswift:
        cr ACK 6c280adcd865ae3da4df53d630c9bf737283a56f
    
    Tree-SHA512: 77792c35679b6c3545fd3a8d3d74c4f515ac2ee9f02d983251aeaaac715d55c122bbb0141abbeac272011f15520b439bd2db4ec8541a58df9b366921d212ca5f

--- src/netaddress.cpp
+++ src/netaddress.cpp
@@ -542,7 +542,7 @@ static std::string IPv4ToString(Span<const uint8_t> a)
     return strprintf("%u.%u.%u.%u", a[0], a[1], a[2], a[3]);
 }
 
-static std::string IPv6ToString(Span<const uint8_t> a)
+static std::string IPv6ToString(Span<const uint8_t> a, uint32_t scope_id)
 {
     assert(a.size() == ADDR_IPV6_SIZE);
     // clang-format off
@@ -586,14 +586,14 @@ std::string CNetAddr::ToStringIP(bool fUseGetnameinfo) const
                     return std::string(name);
             }
         }
-        return IPv6ToString(m_addr);
+        return IPv6ToString(m_addr, m_scope_id);
     }
     case NET_ONION:
         return OnionToString(m_addr);
     case NET_I2P:
         return EncodeBase32(m_addr, false /* don't pad with = */) + ".b32.i2p";
     case NET_CJDNS:
-        return IPv6ToString(m_addr);
+        return IPv6ToString(m_addr, 0);
     case NET_INTERNAL:
         return EncodeBase32(m_addr) + ".internal";
     case NET_UNROUTABLE: // m_net is never and should not be set to NET_UNROUTABLE
