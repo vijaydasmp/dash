2773661bd04f648adb4c45d036401ef14b88e016
Konstantin Akimov <github@knstqq.com>
2022-04-28 14:56:27 +0100

    Merge bitcoin/bitcoin#24322: [kernel 1/n] Introduce initial `libbitcoinkernel`
    BACKPORT NOTE:
    build: Don't add unrelated libs to LIBTEST_* --- done via a3c11b679bea0e84f2bf96ac76e998b50b66eab8
    
    035fa1f07aacb7bce74c0884ae28c8cf00fe3b1b build: Remove LIBTOOL_APP_LDFLAGS for bitcoin-chainstate (Cory Fields)
    3f0595095dd6d230dc661641227937e3ab4ca8d3 docs: Add libbitcoinkernel_la_SOURCES explanation (Carl Dong)
    94ad45deb257a95b4e98aa85da0371fb072fcd4c ci: Build libbitcoinkernel (Carl Dong)
    26b2e7ffb3471a4712e5b9e50e066e0e3218f0dd build: Extract the libbitcoinkernel library (Carl Dong)
    1df44dd20ca9e6e55eb353824b27d11bd1878c59 b-cs: Define G_TRANSLATION_FUN in bitcoinkernel.cpp (Carl Dong)
    83a0bb7cc9907dbe089409ed5a417277ed63ed95 build: Separate lib_LTLIBRARIES initialization (Carl Dong)
    c1e16cb31f4d8edde8fea310011189b8b272cb07 build: Create .la library for bitcoincrypto (Carl Dong)
    8bdfe057c796dde1cd2e5a37a73e87a879e9fe56 build: Create .la library for leveldb (Carl Dong)
    05d1525b6d4412f68ff4c5460cd1daa6fb49969b build: Create .la library for crc32c (Carl Dong)
    64caf944797bc35c3044fe5675389656f9511a41 build: Remove vestigial LIBLEVELDB_SSE42 (Carl Dong)
    1392e8e2d8cfe4115f0a152aca16ffe3f0f4573a build: Don't add unrelated libs to LIBTEST_* (Carl Dong)
    
    Pull request description:
    
      Part of: #24303
    
      This PR introduces a `libbitcoinkernel` static library linking in the minimal list of files necessary to use our consensus engine as-is. `bitcoin-chainstate` introduced in #24304 now will link against `libbitcoinkernel`.
    
      Most of the changes are related to the build system.
    
      Please read the commit messages for more details.
    
    ACKs for top commit:
      theuni:
        This may be my favorite PR ever. It's a privilege to ACK 035fa1f07aacb7bce74c0884ae28c8cf00fe3b1b.
    
    Tree-SHA512: b755edc3471c7c1098847e9b16ab182a6abb7582563d9da516de376a770ac7543c6fdb24238ddd4d3d2d458f905a0c0614b8667aab182aa7e6b80c1cca7090bc
    
    Co-authored-by: fanquake <fanquake@gmail.com>

--- src/bitcoin-chainstate.cpp
+++ src/bitcoin-chainstate.cpp
@@ -36,40 +36,6 @@
 #include <functional>
 #include <iosfwd>
 
-const std::function<std::string(const char*)> G_TRANSLATION_FUN = nullptr;
-
-//----------------
-// symbols g_stats_client, ValueFromAmount, GetPrettyExceptionStr are re-defined
-// here especially for dash-chainstate binary (kernel), because adding sources
-// containing them pulls too many extra dependencies recursively
-#include <stats/client.h>
-std::unique_ptr<StatsdClient> g_stats_client{std::make_unique<StatsdClient>()};
-
-UniValue ValueFromAmount(const CAmount amount)
-{
-    static_assert(COIN > 1);
-    int64_t quotient = amount / COIN;
-    int64_t remainder = amount % COIN;
-    if (amount < 0) {
-        quotient = -quotient;
-        remainder = -remainder;
-    }
-    return UniValue(UniValue::VNUM,
-            strprintf("%s%d.%08d", amount < 0 ? "-" : "", quotient, remainder));
-}
-std::string GetPrettyExceptionStr(const std::exception_ptr& e)
-{
-    try {
-        // rethrow and catch the exception as there is no other way to reliably cast to the real type (not possible with RTTI)
-        std::rethrow_exception(e);
-    } catch (const std::exception& e2) {
-        return e2.what();
-    } catch (...) {
-        throw;
-    }
-}
-//////////////////////
-
 int main(int argc, char* argv[])
 {
     // SETUP: Argument parsing and handling
