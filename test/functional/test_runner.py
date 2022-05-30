13336e83ec02ae0d53e3388cf44d58544b387875
Konstantin Akimov <github@knstqq.com>
2022-05-30 08:52:03 +0200

    Merge bitcoin/bitcoin#25204: rpc: remove deprecated top-level fee fields from mempool entries
    885694d7941ccb601388bc8412ac1df182785ef8 doc: add release note about removal of `deprecatedrpc=fees` flag (Sebastian Falbesoner)
    387ae8bc09ac211af4a4e45ea4ff22518d541d0b rpc: remove deprecated fee fields from mempool entries (Sebastian Falbesoner)
    
    Pull request description:
    
      Deprecating the top-level fee fields (`fee`, `modifiedfee`, `ancestorfees` and `descendantfees`) from the mempool entries and introducing `-deprecatedrpc=fees` was done in PR #22689 (released in v23.0). For the next release v24.0, this configuration option can be removed.
    
    ACKs for top commit:
      fanquake:
        ACK 885694d7941ccb601388bc8412ac1df182785ef8
    
    Tree-SHA512: fec6b5be5c3f0cd55738a888b390ef9271e70b2dba913a14ce82427dac002e999f93df298bb3b494f3d1b850a23d2b5b3e010e901543b0d18db9be133579e1ec
    
    Co-authored-by: MacroFake <falke.marco@gmail.com>

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -412,7 +412,6 @@ BASE_SCRIPTS = [
     'feature_settings.py',
     'rpc_getdescriptorinfo.py',
     'rpc_getpeerinfo_deprecation.py',
-    'rpc_mempool_entry_fee_fields_deprecation.py',
     'rpc_mempool_info.py',
     'rpc_help.py',
     'feature_dirsymlinks.py',
