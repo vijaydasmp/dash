4755b4e3c543fbd6dc2a61c047642c0b26a1fcaf
Ava Chow <github@achow101.com>
2024-02-19 13:07:56 -0500

    Merge bitcoin/bitcoin#29434: rpc: Fixed signed integer overflow for large feerates
    dddd7be9bf038c25f3e53c5bd708fb9cf73d4493 doc: Clarify maxfeerate help (MarcoFalke)
    fa2a4fdef779b01e847def5985deafedc6dd3da8 rpc: Fixed signed integer overflow for large feerates (MarcoFalke)
    fade94d11a5b93113975c4b2f62a357a70d03191 rpc: Add ParseFeeRate helper (MarcoFalke)
    fa0ff6610944fdda716fb0134b78cb85a4c9c26d rpc: Implement RPCHelpMan::ArgValue<> for UniValue (MarcoFalke)
    
    Pull request description:
    
      Passing large BTC/kvB feerates to RPCs is problematic, because:
    
      * They are likely a typo. 1BTC/kvB (or larger) seems absurd.
      * They may cause signed integer overflow.
      * Anyone really wanting to pick such a large value can set `0` to disable the check.
    
      Fix all issues by rejecting anything more than 1BTC/kvB during parsing.
    
    ACKs for top commit:
      brunoerg:
        crACK dddd7be9bf038c25f3e53c5bd708fb9cf73d4493
      achow101:
        ACK dddd7be9bf038c25f3e53c5bd708fb9cf73d4493
      vasild:
        ACK dddd7be9bf038c25f3e53c5bd708fb9cf73d4493
      tdb3:
        Code review ACK and basic test ACK for dddd7be9bf038c25f3e53c5bd708fb9cf73d4493.
      fjahr:
        utACK dddd7be9bf038c25f3e53c5bd708fb9cf73d4493
    
    Tree-SHA512: 5dcce1f0abe059dc6b2ff56787e11081d73a45b4ddd6dcc2c1ea13709ebc13af5e7265e84fffb97ef32027b56b81955672a67ed7702e8fa30c2e849d67727bac

--- src/rpc/mempool.cpp
+++ src/rpc/mempool.cpp
@@ -46,7 +46,7 @@ RPCHelpMan sendrawtransaction()
             {"hexstring", RPCArg::Type::STR_HEX, RPCArg::Optional::NO, "The hex string of the raw transaction"},
             {"maxfeerate", RPCArg::Type::AMOUNT, RPCArg::Default{FormatMoney(DEFAULT_MAX_RAW_TX_FEE_RATE.GetFeePerK())},
              "Reject transactions whose fee rate is higher than the specified value, expressed in " + CURRENCY_UNIT +
-                 "/kB.\nSet to 0 to accept any fee rate.\n"},
+                 "/kB.\nFee rates larger than 1BTC/kvB are rejected.\nSet to 0 to accept any fee rate.\n"},
             {"instantsend", RPCArg::Type::BOOL, RPCArg::Optional::OMITTED, "Deprecated and ignored"},
             {"bypasslimits", RPCArg::Type::BOOL, RPCArg::Default{false}, "Bypass transaction policy limits"},
         },
@@ -78,9 +78,7 @@ RPCHelpMan sendrawtransaction()
             }
             CTransactionRef tx(MakeTransactionRef(std::move(mtx)));
 
-            const CFeeRate max_raw_tx_fee_rate = request.params[1].isNull() ?
-                                                     DEFAULT_MAX_RAW_TX_FEE_RATE :
-                                                     CFeeRate(AmountFromValue(request.params[1]));
+            const CFeeRate max_raw_tx_fee_rate{ParseFeeRate(self.Arg<UniValue>(1))};
 
             int64_t virtual_size = GetVirtualTransactionSize(*tx);
             CAmount max_raw_tx_fee = max_raw_tx_fee_rate.GetFee(virtual_size);
@@ -116,7 +114,8 @@ static RPCHelpMan testmempoolaccept()
                 },
             },
             {"maxfeerate", RPCArg::Type::AMOUNT, RPCArg::Default{FormatMoney(DEFAULT_MAX_RAW_TX_FEE_RATE.GetFeePerK())},
-             "Reject transactions whose fee rate is higher than the specified value, expressed in " + CURRENCY_UNIT + "/kB\n"},
+             "Reject transactions whose fee rate is higher than the specified value, expressed in " + CURRENCY_UNIT +
+                 "/kB\nFee rates larger than 1BTC/kvB are rejected.\nSet to 0 to accept any fee rate."},
         },
         RPCResult{
             RPCResult::Type::ARR, "", "The result of the mempool acceptance test for each raw transaction in the input array.\n"
@@ -160,9 +159,7 @@ static RPCHelpMan testmempoolaccept()
                                    "Array must contain between 1 and " + ToString(MAX_PACKAGE_COUNT) + " transactions.");
             }
 
-            const CFeeRate max_raw_tx_fee_rate = request.params[1].isNull() ?
-                                                     DEFAULT_MAX_RAW_TX_FEE_RATE :
-                                                     CFeeRate(AmountFromValue(request.params[1]));
+            const CFeeRate max_raw_tx_fee_rate{ParseFeeRate(self.Arg<UniValue>(1))};
 
             std::vector<CTransactionRef> txns;
             txns.reserve(raw_transactions.size());
