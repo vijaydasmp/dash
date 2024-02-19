ed95384919fa9187bf0c7b66aefe8efc5cfd164c
Ava Chow <github@achow101.com>
2024-02-19 13:07:56 -0500

    Merge bitcoin/bitcoin#29434: rpc: Fixed signed integer overflow for large feerates
    dddd7be9bf038c25f3e53c5bd708fb9cf73d4493 doc: Clarify maxfeerate help (MarcoFalke)
    fa2a4fdef779b01e847def5985deafedc6dd3da8 rpc: Fixed signed integer overflow for large feerates (MarcoFalke)
    fade94d11a5b93113975c4b2f62a357a70d03191 rpc: Add ParseFeeRate helper (MarcoFalke)
    fa0ff6610944fdda716fb0134b78cb85a4c9c26d rpc: Implement RPCHelpMan::ArgValue<> for UniValue (MarcoFalke)
    
    Pull request description:
    
      Passing large BTC/kvB feerates to RPCs is problematic, because:
    
      * They are likely a typo. 1BTC/kvB (or larger) seems absurd.
      * They may cause signed integer overflow.
      * Anyone really wanting to pick such a large value can set `0` to disable the check.
    
      Fix all issues by rejecting anything more than 1BTC/kvB during parsing.
    
    ACKs for top commit:
      brunoerg:
        crACK dddd7be9bf038c25f3e53c5bd708fb9cf73d4493
      achow101:
        ACK dddd7be9bf038c25f3e53c5bd708fb9cf73d4493
      vasild:
        ACK dddd7be9bf038c25f3e53c5bd708fb9cf73d4493
      tdb3:
        Code review ACK and basic test ACK for dddd7be9bf038c25f3e53c5bd708fb9cf73d4493.
      fjahr:
        utACK dddd7be9bf038c25f3e53c5bd708fb9cf73d4493
    
    Tree-SHA512: 5dcce1f0abe059dc6b2ff56787e11081d73a45b4ddd6dcc2c1ea13709ebc13af5e7265e84fffb97ef32027b56b81955672a67ed7702e8fa30c2e849d67727bac

--- test/functional/mempool_accept.py
+++ test/functional/mempool_accept.py
@@ -82,9 +82,17 @@ class MempoolAcceptanceTest(BitcoinTestFramework):
             rawtxs=[raw_tx_in_block],
             maxfeerate=-0.01,
         ))
+        # Also check feerate. 1DASH/kB fails
+        assert_raises_rpc_error(-8, "Fee rates larger than or equal to 1DASH/kB are not accepted", lambda: self.check_mempool_result(
+            result_expected=None,
+            rawtxs=[raw_tx_in_block],
+            maxfeerate=1,
+        ))
+        # ... 0.99 passes
         self.check_mempool_result(
             result_expected=[{'txid': txid_in_block, 'allowed': False, 'reject-reason': 'txn-already-known'}],
             rawtxs=[raw_tx_in_block],
+            maxfeerate=0.99,
         )
 
         self.log.info('A transaction not in the mempool')
