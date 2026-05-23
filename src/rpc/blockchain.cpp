69dd6357f0584dd1fea4aa97c8ed48a3182d2af5
Konstantin Akimov <github@knstqq.com>
2026-05-23 15:35:15 +0700

    refactor: remove Bitcoin's specific hashes for BIP30/BIP34
    Workaround implementation stays on place to reduce further conflicts

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -2164,9 +2164,9 @@ static RPCHelpMan getblockstats()
                 size_t out_size = GetSerializeSize(out, PROTOCOL_VERSION) + PER_UTXO_OVERHEAD;
                 utxo_size_inc += out_size;
 
-                // The Genesis block and the repeated BIP30 block coinbases don't change the UTXO
+                // The Genesis block coinbase don't change the UTXO
                 // set counts, so they have to be excluded from the statistics
-                if (pindex.nHeight == 0 || (IsBIP30Repeat(pindex) && tx->IsCoinBase())) continue;
+                if (pindex.nHeight == 0) continue;
                 // Skip unspendable outputs since they are not included in the UTXO set
                 if (out.scriptPubKey.IsUnspendable()) continue;
 
