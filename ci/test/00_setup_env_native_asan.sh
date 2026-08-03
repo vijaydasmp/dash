0f7da3afe2a5ee9b98c6f0610a5bd4b719558944
Konstantin Akimov <github@knstqq.com>
2026-08-04 03:04:50 +0700

    fix: use only 2 parallel workers for asan job

--- ci/test/00_setup_env_native_asan.sh
+++ ci/test/00_setup_env_native_asan.sh
@@ -10,7 +10,7 @@ export CONTAINER_NAME=ci_native_asan
 export PACKAGES="clang-19 llvm-19 libclang-rt-19-dev python3-zmq qtbase5-dev qttools5-dev-tools libevent-dev bsdmainutils libboost-dev libdb5.3++-dev libminiupnpc-dev libzmq3-dev libqrencode-dev"
 # Reuses the depends built for the linux64 target, which uses the defaults.
 export DEP_OPTS=""
-export TEST_RUNNER_EXTRA="--timeout-factor=4"  # Increase timeout because sanitizers slow down
+export TEST_RUNNER_EXTRA="--timeout-factor=4 -j2"  # Increase timeout because sanitizers slow down
 export GOAL="install"
 export BITCOIN_CONFIG="--enable-zmq --enable-crash-hooks --with-gui=qt5 \
 --with-sanitizers=address,float-divide-by-zero,integer,undefined \
