ef7ba7b9772cf851ec8e7c016dd05a5f3b174e25
Konstantin Akimov <github@knstqq.com>
2026-08-20 00:03:26 +0700

    fix: add one more splitted module: qt/guiutil + qqt/guiutil_font
    The split between modules to avoid conflicts for backports which changes qt/guiutil

--- contrib/devtools/circular-dependencies.py
+++ contrib/devtools/circular-dependencies.py
@@ -13,6 +13,7 @@ MAPPING = {
     'core_write.cpp': 'core_io.cpp',
     'evo/core_write.cpp': 'core_io.cpp',
     'llmq/core_write.cpp': 'core_io.cpp',
+    'qt/guiutil_font.cpp': 'qt/guiutil.cpp',
 }
 
 # Directories with header-based modules, where the assumption that .cpp files
